44d17e05f49085f2a1b2a63236d892b549999605
benesjan <janbenes1234@gmail.com>
2022-01-11 16:44:37 +0100

    test: full deposit withdrawal flow

--- src/test/StabilityPoolBridge.t.sol
+++ src/test/StabilityPoolBridge.t.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-only
 pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
 
 import "../StabilityPoolBridge.sol";
+import "../Types.sol";
 import "./TestUtil.sol";
 
 contract StabilityPoolBridgeTest is TestUtil {
@@ -10,8 +12,8 @@ contract StabilityPoolBridgeTest is TestUtil {
     function setUp() public {
         setUpTokens();
 
-        address rollupProcessor = address(0);
-        address stabilityPool = 0x5f98805A4E8be255a32880FDeC7F6728C6568bA0;
+        address rollupProcessor = address(this);
+        address stabilityPool = 0x66017D22b0f8556afDd19FC67041899Eb65a21bb;
         address frontEndTag = address(0);
 
         stabilityPoolBridge = new StabilityPoolBridge(rollupProcessor, tokens["LUSD"].addr, stabilityPool, frontEndTag);
@@ -21,5 +23,53 @@ contract StabilityPoolBridgeTest is TestUtil {
         assertEq(stabilityPoolBridge.name(), "StabilityPoolBridge");
         assertEq(stabilityPoolBridge.symbol(), "SPB");
         assertEq(uint256(stabilityPoolBridge.decimals()), 18);
+    }
+
+    function test_fullDepositWithdrawalFlow() public {
+        // I will deposit and withdraw 1 million LUSD
+        uint depositAmount = 10 ** 6 * 10 ** 18;
+
+        // 1. mint LUSD to this contract
+        mint("LUSD", address(this), depositAmount);
+
+        // 2. Allow StabilityPoolBridge to spend LUSD of this
+        IERC20(tokens["LUSD"].addr).approve(address(stabilityPoolBridge), depositAmount);
+
+        // 3. Deposit LUSD to StabilityPool through the bridge
+        stabilityPoolBridge.convert(
+            Types.AztecAsset(1, tokens["LUSD"].addr, Types.AztecAssetType.ERC20),
+            Types.AztecAsset(0, address(0), Types.AztecAssetType.NOT_USED),
+            Types.AztecAsset(2, address(stabilityPoolBridge), Types.AztecAssetType.ERC20),
+            Types.AztecAsset(0, address(0), Types.AztecAssetType.NOT_USED),
+            depositAmount,
+            0,
+            0
+        );
+
+        // 4. Check the total supply of SPB token is equal to the amount of LUSD deposited
+        assertEq(stabilityPoolBridge.totalSupply(), depositAmount);
+
+        // 5. Check the SPB balance of this is equal to the amount of LUSD deposited
+        assertEq(stabilityPoolBridge.balanceOf(address(this)), depositAmount);
+
+        // 6. Check the LUSD balance of StabilityPoolBridge in StabilityPool is equal to the amount of LUSD deposited
+        assertEq(stabilityPoolBridge.stabilityPool().getCompoundedLUSDDeposit(address(stabilityPoolBridge)), depositAmount);
+
+        // 7. Withdraw LUSD from StabilityPool through the bridge
+        stabilityPoolBridge.convert(
+            Types.AztecAsset(2, address(stabilityPoolBridge), Types.AztecAssetType.ERC20),
+            Types.AztecAsset(0, address(0), Types.AztecAssetType.NOT_USED),
+            Types.AztecAsset(1, tokens["LUSD"].addr, Types.AztecAssetType.ERC20),
+            Types.AztecAsset(0, address(0), Types.AztecAssetType.NOT_USED),
+            depositAmount,
+            0,
+            0
+        );
+
+        // 8. Check the total supply of SPB token is 0
+        assertEq(stabilityPoolBridge.totalSupply(), 0);
+
+        // 9. Check the LUSD balance of this contract is equal to the initial LUSD deposit
+        assertEq(IERC20(tokens["LUSD"].addr).balanceOf(address(this)), depositAmount);
     }
 }
